ad688dd54cfb94190b81cfa10e723104019e2cc2
olutukko <rasmus.mannik@gmail.com>
2024-04-29 01:31:50 +0300

    added api functionality back and fixed the rendering and fetching issues

--- mealful/services/RecipesInfoService.js
+++ mealful/services/RecipesInfoService.js
@@ -8,9 +8,11 @@ class RecipesInfoService {
 
   async fetchRecipeById(id) {
     const url = `${this.apiUrl}/${id}/information?apiKey=${this.apiKey}`;
+    console.log(url);
     try {
       const response = await fetch(url);
       const data = await response.json();
+      console.log('Fetched recipe data:', data);
       // Create a new RecipeInfo object with the necessary data
       const recipeInfo = new RecipeInfo({
         id: data.id,
--- mealful/utils/RecipeList.js
+++ mealful/utils/RecipeList.js
@@ -1,6 +1,3 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
 class RecipeList {
   constructor(id, title, image) {
     this.id = id;
@@ -10,54 +7,3 @@ class RecipeList {
 }
 
 export default RecipeList;
-
-/*
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: [],
-    };
-  }
-
-  componentDidMount() {
-    this.fetchRecipes();
-  }
-
-  fetchRecipes = () => {
-    const API_KEY = 'YOUR_SPOONACULAR_API_KEY';
-    const API_URL = `https:/api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}`;
-
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        const recipes = data.results.map((result) => {
-          return new Recipe(
-            result.title,
-            result.extendedIngredients.map((ingredient) => ingredient.name),
-            result.instructions
-          );
-        });
-        this.setState({ recipes });
-      })
-      .catch((error) => console.error('Error fetching recipes:', error));
-  };
-
-  render() {
-    return (
-      <View>
-        <Text>Recipes:</Text>
-        {this.state.recipes.map((recipe, index) => (
-          <View key={index}>
-            <Text>Title: {recipe.title}</Text>
-            <Text>Ingredients: {recipe.ingredients.join(', ')}</Text>
-            <Text>Instructions: {recipe.instructions}</Text>
-          </View>
-        ))}
-      </View>
-    );
-  }
-}
-
-export default App;
-*/
